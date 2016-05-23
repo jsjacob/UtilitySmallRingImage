fad37e6ab4a61296c42674a11278ccf1b8d078d3
John S. Jacob <jsjacob@iamnota.com>
2016-05-23 14:55:52 -0700

    Initial Commit

---
+++ UtilitySmallRingImage WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  UtilitySmallRingImage WatchKit Extension
+//
+//  Created by John S. Jacob on 2016-05-23.
+//  Copyright © 2016 John S. Jacob. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ UtilitySmallRingImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UtilitySmallRingImage
+//
+//  Created by John S. Jacob on 2016-05-23.
+//  Copyright © 2016 John S. Jacob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
